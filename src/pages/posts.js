135b771feb33b231ec02e12d4e251af4899a027a
keremcanb <keremcan@gmail.com>
2020-12-09 01:16:14 +0300

    posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,9 +3,42 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 import Hero from '../components/Hero';
 import Posts from '../components/Posts';
-// ...GatsbyImageSharpFluid
-const PostsPage = ({ data }) => {
-  return <h2>posts page</h2>;
-};
+
+const PostsPage = ({
+  data: {
+    allMdx: { nodes: posts }
+  }
+}) => (
+  <Layout>
+    <Hero />
+    <Posts posts={posts} title="all posts" />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        id
+        frontmatter {
+          title
+          readTime
+          category
+          date(formatString: "MMMM Do, YYYY")
+          author
+          slug
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default PostsPage;
