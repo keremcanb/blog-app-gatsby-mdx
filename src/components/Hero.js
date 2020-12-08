c687c4a12ae8eb736442afa452e4c2b860ef218a
keremcanb <keremcan@gmail.com>
2020-12-08 21:02:15 +0300

    hero comp.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
-// ...GatsbyImageSharpFluid
 
-const Hero = () => <h2>hero component</h2>;
+const query = graphql`
+  {
+    person: file(relativePath: { eq: "person.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const Hero = ({ showPerson }) => {
+  const {
+    person: {
+      childImageSharp: { fluid }
+    }
+  } = useStaticQuery(query);
+
+  return (
+    <header className="hero">
+      {showPerson && <Image fluid={fluid} className="hero-person" />}
+    </header>
+  );
+};
 
 export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,12 @@ import Hero from '../components/Hero';
 import Layout from '../components/Layout/Layout';
 import Posts from '../components/Posts';
 import SEO from '../components/SEO';
+
 // ...GatsbyImageSharpFluid
-const IndexPage = () => <Layout />;
+const IndexPage = () => (
+  <Layout>
+    <Hero showPerson />
+  </Layout>
+);
 
 export default IndexPage;
